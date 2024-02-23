e67996056fd812cd6f0e46f1242115880164d037
impallab <pallab13nayagram@gmail.com>
2024-02-23 18:35:31 +0530

    Performed clean up file and understand the flow

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+import FirstCode from "./First";
+function App() {
+  return (
+    <>
+      <h2>Learn React</h2>
+      <FirstCode />
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/First.jsx
@@ -0,0 +1,21 @@
+function FirstCode(){
+    return(
+        <>
+        <h2>Passing my brief information through components :</h2>
+        <table style={{'border':'3px solid black'}}>
+            <tr>
+                <th style={{'border':'1px solid blue'}}>Name</th>
+                <th style={{'border':'1px solid blue'}}>Mobile No:</th>
+            </tr >
+            
+            <tr style={{'border':'1px solid blue'}}> 
+                <td style={{'border':'1px solid blue'}}>Pallab</td>
+                <td style={{'border':'1px solid blue'}}>9641111733</td>
+            </tr>
+        </table>
+        <a href="https://pallab-portfolio.netlify.app/">PORTFOLIO</a>
+        </>
+    )
+}
+
+export default FirstCode
